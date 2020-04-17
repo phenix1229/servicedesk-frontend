8cb99d09dab969705617ef03b8ffc2da282cdb51
phenix1229 <paul.garay@codeimmersives.com>
2020-04-17 15:04:17 -0400

    base index and app js files

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React , {Component} from 'react';
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+
+        };
+    };
+    render(){
+        return (
+            <h1>hello</h1>
+        )
+    }
+};
+
+export default App;
